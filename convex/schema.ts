cb65c28e1164f5ac37d2dea6077f06d46a6cd382
Arbab-Mustafa <22-SE-58@students.uettaxila.edu.pk>
2024-08-21 02:00:28 +0500

    add setup of conves

---
+++ convex/schema.ts
@@ -0,0 +1,12 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+export default defineSchema({
+  tasks: defineTable({
+    text: v.string(),
+    completed: v.boolean(),
+  }),
+  products: defineTable({
+    name: v.string(),
+    price: v.number(),
+  }),
+});
